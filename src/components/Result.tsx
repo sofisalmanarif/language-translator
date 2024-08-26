a6a5d5059d51d2fb18ca70dec8cc76af59eff327
sofisalmanarif <sofisalman9906@gmail.com>
2024-08-26 21:04:39 +0530

    result page

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Result = () => {
   return (
-    <div>Result</div>
-  )
+    <div className="flex flex-col items-center justify-center">
+         <h1 className="text-3xl font-extrabold">Result</h1>
+            <p>you got 3 out of 5</p>
+         <div className='flex gap-20'>
+          <div>Your Ans</div>
+          <div> correct And</div>
+         </div>
+         </div>
+     )
+  
 }
 
 export default Result
